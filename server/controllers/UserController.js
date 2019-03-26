c5175d430b85c231e10b938d950599ca09551905
fedex2018 <federico@grayshirts.com.ar>
2019-03-26 17:31:32 -0300

    email validated on user creation

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -4,34 +4,21 @@ let errorService = require('../services/errorService')
 let responseService = require('../services/responseService')
 let validatorService = require('../services/validatorService')
 
-const user = require('../models/user');
+const User = require('../models/user');
 
 class UserController {
-
   constructor() {
 
   }
 
-  async create(req,res,next){
+  create(req,res,next){
       req.getValidationResult() // to get the result of above validate fn
           .then( result => validatorService.validationHandler(result))
-          .then(() => {
-
-              res.send(req.body);                        
-              return
-              const { name, surname, nickname, password } = req.body
+          .then(async () => {
               
-              User.create({
-        
-                userName,
-        
-                email,
-        
-                phone,
-        
-                status,   
-              })
-              .then(user => res.json(user))
+              let userCreated = await User.create( User.createData(req.body) )
+              
+              res.send(userCreated)
             })
           .catch(err => {
             console.log("--------------")
@@ -50,11 +37,21 @@ class UserController {
           body('name').isLength({ min: 5 }).withMessage("El nombre debe tener un minimo de 3 caracteres"),
           body('surname').exists().withMessage("El apellido no puede ser nulo"),
           body('nickname').optional().isLength({ min: 3 }).withMessage("El nombre de usuario debe tener al menos 3 caracteres"),
-          body('password').isLength({ min: 3 }).withMessage("El password debe tener al menos 3 caracteres")
+          body('password').isLength({ min: 3 }).withMessage("El password debe tener al menos 3 caracteres"),
+          body('email').custom(value => {
+                              return User.findUserByEmail(value)
+                              .then(user => {                    
+                                if (user) {
+                                  return Promise.reject('E-mail ya utilizado por otro usuario, utilice otro por favor');
+                                }
+
+                              }).catch( err => {throw(err)})
+                            })
         ]
       }
     }
   }
+  
 }
 
 module.exports = UserController
--- server/models/user.js
+++ server/models/user.js
@@ -4,15 +4,48 @@ var mongoose = require('mongoose');
 //Define a schema
 var Schema = mongoose.Schema;
 
-var SchemaUser = new Schema({
-  name              : String,
-  surname           : String,
-  nickname          : String,
-  password          : String,
-  type              : String,
-  status            : String,
-  created           : Date,
+var UserSchema = new Schema({
+  name: {
+    type: String,required: true
+  },
+  surname: {
+    type: String,required: true
+  },
+  nickname : String,
+  password: {
+    type: String,required: true
+  },
+  email: {
+    type: String,required: true
+  },
+  status: {
+    type: String,required: true
+  },
+  type: {
+    type: String,required: true
+  },
+  created: {
+    type: String,required: true
+  }
 });
 
+UserSchema.statics.createData = function(params,type = "USER",status="ACTIVE") {
+  const { name, surname, nickname, password ,email} = params
+              
+  let data = {
+    name,surname,nickname,password,email,
+    type : type,
+    status : status,
+    created : new Date().toUTCString()
+  }
+
+  return data
+}
+
+UserSchema.statics.findUserByEmail = function(mail) {
+  return this.findOne({ email: mail });
+}
+
+
 //Export function to create "SomeModel" model class
-module.exports = mongoose.model('User', SchemaUser );
+module.exports = mongoose.model('User', UserSchema );
